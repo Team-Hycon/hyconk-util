2252418469976b6a6bcea45ad52db058fc9b9c64
Niall Moore <niall@hycon.io>
2019-05-16 16:11:39 +0900

    Added Hycon to string function

--- src/main/kotlin/io/hycon/hyconkUtil.kt
+++ src/main/kotlin/io/hycon/hyconkUtil.kt
@@ -1,10 +1,22 @@
 package io.hycon
 
-data class Language(val name: String, val hotness: Int)
-
 class HyconkUtil {
-    /**
-     * @return data relating to the Kotlin {@code Lanugage}.
-     */
-    fun kotlinLanguage() = Language("Kotlin", 10)
+    fun hyconFromString(valueString: String?):Long {
+        if (valueString == "" || valueString == null) {
+            return 0L
+        } else {
+            var hycString = valueString
+            if (valueString.endsWith('.')) {
+                hycString+="0"
+            }
+            var splitArr = hycString.split('.')
+            var hyc = 0L
+            hyc += splitArr[0].toLong()*1000000000L
+            if (splitArr.size > 1) {
+                val exp = (9-splitArr[1].length).toDouble()
+                hyc += splitArr[1].toLong() * (Math.pow(10.0,exp)).toLong()
+            }
+            return hyc
+        }
+    }
 }
--- src/test/kotlin/io/hycon/kyconKUtilTest.kt
+++ src/test/kotlin/io/hycon/kyconKUtilTest.kt
@@ -4,8 +4,12 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class HyconKUtilTest {
-    @Test fun testMyLanguage() {
-        assertEquals("Kotlin", HyconkUtil().kotlinLanguage().name)
-        assertEquals(10, HyconkUtil().kotlinLanguage().hotness)
+    @Test fun testHyconFromString() {
+        assertEquals(0L, HyconkUtil().hyconFromString(""))
+        assertEquals(1L,HyconkUtil().hyconFromString("0.000000001"))
+        assertEquals(0L,HyconkUtil().hyconFromString(null))
+        assertEquals(1000000000L, HyconkUtil().hyconFromString("1"))
+        assertEquals(1000000000L, HyconkUtil().hyconFromString("1."))
+        assertEquals(1010000000L, HyconkUtil().hyconFromString("1.01"))
     }
 }
